c31649c19f09403d5510918ee36a77d0c9057227
Aryanluthra1911 <aryanluthra1911@gmail.com>
2025-08-29 12:51:04 +0530

    footer responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
             </div>
 
             {isDesktop ? (
-                <div className="flex flex-row justify-center text-4xl md:text-9xl font-bold w-full translate-y-2 h-20 overflow-hidden gap-1 md:gap-4 -tracking-[0.02em]">
+                <div className="flex flex-row justify-center text-2xl sm:text-4xl md:text-9xl font-bold w-full translate-y-2 h-20 overflow-hidden gap-1 md:gap-4 -tracking-[0.02em]">
                     <div>ARYAN..</div>
                     <div className="flex flex-col justify-center items-center text-[#acacac] text-xs mt-8 mb-4 tracking-[0em] font-light w-full">
                         <p>Built with ♡ | © {new Date().getFullYear()} ARYAN LUTHRA</p>
@@ -34,7 +34,7 @@ const Footer = () => {
                         <p>Built with ♡ | © {new Date().getFullYear()} ARYAN LUTHRA</p>
                         <p>Find more of my projects on <a href="https://github.com/Aryanluthra1911" className="text-white font-semibold">Github</a></p>
                     </div>
-                    <div className="flex justify-center text-6xl font-bold w-full translate-y-1 h-10 overflow-hidden gap-6 -tracking-[0.02em]">
+                    <div className="pt-2 sm:pt-0 flex justify-center text-5xl sm:text-6xl font-bold w-full translate-y-1 h-10 overflow-hidden gap-6 -tracking-[0.02em]">
                         <div>ARYAN</div>
                         <div>LUTHRA</div>
                     </div>
